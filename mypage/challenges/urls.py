3e0f89491eae8a215096c67f29796e2fc0537ba4
Ruslan <russam4515@gmail.com>
2024-02-25 19:02:03 -0500

    dynamic

--- mypage/challenges/urls.py
+++ mypage/challenges/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from.import views
 urlpatterns = [
-    path("january",views.index)
+    path("<month>",views.monthly_challenge)
+
 ]
--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("This works")
+
+
+def february(request):
+    return HttpResponse("Februrary works")
+
+
+def monthly_challenge(request,month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "no meat in January"
+    elif month =='february':
+        challenge_text = "no shave February"    
+    else:
+        return  HttpResponseNotFound ( "no Such month supported ")
+        
+    return HttpResponse(challenge_text)
