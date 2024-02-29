c1bf7b423bd3795e5b7a6183dfc5ff1386ad903e
Ruslan <russam4515@gmail.com>
2024-02-29 15:41:53 -0500

    tags

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+# from django.template.loader import render_to_string
 # Create your views here.
 
 monthly_challenges = {"january": "NO meat in Jan",
@@ -23,6 +24,11 @@ def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
 
+        #response_data = render_to_string("challenges/challenge.html")
+        return render(request,"challenges/challenge.html",{"text":challenge_text,
+                                                           "month_name":month.capitalize()
+                                                           })
+
     except:
         return HttpResponseNotFound("no Such month supported ")
-    return HttpResponse(challenge_text)
+    
