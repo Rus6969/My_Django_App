9b3c95f40db7b0f6b5814e969868da7c3855160f
Ruslan <russam4515@gmail.com>
2024-03-19 14:42:50 -0400

    new tag has been added

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-# is used to redirect path from urls to views for dynamic solutions 
+# is used to redirect path from urls to views for dynamic solutions
 from django.urls import reverse
 
 from django.template.loader import render_to_string
 
- 
 
 monthly_challenges = {
 
@@ -36,61 +35,33 @@ monthly_challenges = {
 
 }
 
- 
 
 # Create your views here.
 
- 
 
 def index(request):
 
-    list_items = ""
-
     months = list(monthly_challenges.keys())
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
- 
-
-    for month in months:
-
-        capitalized_month = month.capitalize()
-
-        month_path = reverse("month-challenge", args=[month])
-
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
- 
-
-    # "<li><a href="...">January</a></li><li><a href="...">February</a></li>..."
-
- 
-
-    response_data = f"<ul>{list_items}</ul>"
-
-    return HttpResponse(response_data)
-
- 
 
 def monthly_challenge_by_number(request, month):
 
     months = list(monthly_challenges.keys())
 
- 
-
     if month > len(months):
 
         return HttpResponseNotFound("Invalid month")
 
- 
-
     redirect_month = months[month - 1]
-     # handles diferences in urls (challenge!= challenges )
-    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
+    # handles diferences in urls (challenge!= challenges )
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # /challenge/january
 
     return HttpResponseRedirect(redirect_path)
 
- 
-
- 
 
 def monthly_challenge(request, month):
 
@@ -98,9 +69,9 @@ def monthly_challenge(request, month):
 
         challenge_text = monthly_challenges[month]
 
-        response_data = render(request,"challenges/challenge.html",{
-            "text":challenge_text
-           
+        response_data = render(request, "challenges/challenge.html", {
+            "text": challenge_text
+
         })
 
         return HttpResponse(response_data)
