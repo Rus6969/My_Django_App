3f5ce3d122a26d0ddb3dff9ef27b3a3e7239d489
Ruslan <russam4515@gmail.com>
2024-03-22 14:46:59 -0400

    nav is added:

--- mypage/challenges/urls.py
+++ mypage/challenges/urls.py
@@ -10,7 +10,7 @@ from.import views
 # path name to URL for constructing paths 
 urlpatterns = [
     # this empty path is created to create URL for /challenges
-    path("",views.index),
+    path("",views.index,name="index"),
     path("<int:month>",views.monthly_challenge_by_number),
     # we gave name here to our path "name = month-challege " to implement redirection in views 
     path("<str:month>",views.monthly_challenge,name="month-challenge")
